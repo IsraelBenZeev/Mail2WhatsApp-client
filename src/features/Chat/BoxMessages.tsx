e8ea3843ef8e7bc108d7b5b7101d06e57c65bba9
IsraelBenZeev <ibz0502291330@gmail.com>
2025-11-03 00:35:28 +0200

    Restore files

---
+++ src/features/Chat/BoxMessages.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { InputChat } from './InputChat';
+import { MessageItem } from './MessageItem';
+import type { MessageType } from '../../types/MessageType';
+
+export const BoxMessages = () => {
+  const [value, setValue] = useState<string>('');
+  const [responseLLm, setResponseLLm] = useState<string>('');
+  const [messagesOwn, setMessagesOwn] = useState<MessageType[]>([]);
+  const [status, setStatus] = useState<
+    'idle' | 'sending' | 'sent' | 'loading' | 'success' | 'error'
+  >('idle');
+  console.log('value:', value);
+
+  return (
+    <div className="flex flex-col justify-end border gap-1 border-black px-2 py-1 w-full h-screen">
+      <div className='overflow-y-auto'>
+        {messagesOwn.map((msg, index) => (
+          <MessageItem
+            key={index}
+            label={msg.content}
+            time={msg.time || ''}
+            isOwn={msg.role === 'user'}
+          />
+        ))}
+      </div>
+      <InputChat
+        value={value}
+        setValue={setValue}
+        setMessages={setMessagesOwn}
+        setStatus={setStatus}
+        status={status}
+      />
+    </div>
+  );
+};
---
+++ src/features/Chat/InputChat.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState, type FC } from 'react';
+import { LuSendHorizontal } from 'react-icons/lu';
+import { sendMessage } from '../../utils/service';
+import type { MessageType } from '../../types/MessageType';
+import { FiLoader } from 'react-icons/fi';
+
+type InputChatProps = {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  setMessages: React.Dispatch<React.SetStateAction<MessageType[]>>;
+  setStatus: React.Dispatch<
+    React.SetStateAction<
+      'idle' | 'sending' | 'sent' | 'loading' | 'success' | 'error'
+    >
+  >;
+  status: 'idle' | 'sending' | 'sent' | 'loading' | 'success' | 'error';
+};
+export const InputChat: FC<InputChatProps> = ({
+  value,
+  setValue,
+  setMessages,
+  setStatus,
+  status,
+}) => {
+  const addMessageUser = () => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        role: 'user',
+        content: value,
+        time: new Date().toLocaleTimeString(),
+      },
+    ]);
+    setValue('');
+  };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      if (value.trim() === '') {
+        console.log('הודעה ריקה');
+        return;
+      }
+      addMessageUser();
+      sendMessage(value, setStatus, setMessages);
+    }
+  };
+  return (
+    <div className="flex flex-col gap-1">
+      {status === 'loading' && <div className="self-end loader"></div>}
+      {/* {true && <div className="self-end loader"></div>} */}
+      <div className="flex gap-2 w-full border border-grayDark p-1 rounded-lg bg-white">
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          type="text"
+          className="w-full block border-none outline-none text-right text-xs"
+          onKeyDown={handleKeyDown}
+        />
+        <LuSendHorizontal
+          className="rotate-180"
+          onClick={() => {
+            if (value.trim() === '') {
+              console.log('הודעה ריקה');
+              return;
+            }
+            addMessageUser();
+            sendMessage(value, setStatus, setMessages);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
