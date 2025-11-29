919e7aef036d6d3a9adca6458d9eb2c836f83412
IsraelBenZeev <ibz0502291330@gmail.com>
2025-11-30 01:14:36 +0200

    2

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || '',
+  server: {
+    port: 5173,
+    allowedHosts: ['.localhost', 'grumpy-grapes-guess.loca.lt'],
+  },
 })
